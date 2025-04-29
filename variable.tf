6cfbc432745fcaf8552d9a093ee2c2d5e4f6f0d8
Aldo <livio.gieraldo@izeno.com>
2025-04-29 12:06:18 +0700

    initial commit

---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "ami_id" {
+  description = "AMI ID to use"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Instance name tag"
+  type        = string
+  default     = "demo-instance"
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair name (already created in AWS)"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
