4b11c74057804e7ad2687dcdf52935358ebc1fce
Aldo <livio.gieraldo@izeno.com>
2025-05-05 13:53:30 +0700

    test1

--- main.tf
+++ main.tf
@@ -10,18 +10,6 @@ data "aws_vpc" "existing" {
   }
 }
 
-# Subnet baru dibuat di VPC yang sudah ada
-resource "aws_subnet" "example_subnet" {
-  vpc_id                  = data.aws_vpc.existing.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[0]
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "example-subnet"
-  }
-}
-
 # Assign existing security group 
 resource "aws_instance" "example" {
   ami                    = var.ami_id
