17f3a1b4c77edef96e1401dacbb15de3a1fc1ea6
Aldo <livio.gieraldo@izeno.com>
2025-05-05 13:43:44 +0700

    ts1

--- main.tf
+++ main.tf
@@ -2,12 +2,58 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id                  = aws_vpc.example_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "example-subnet"
+  }
+}
+
+resource "aws_security_group" "example_sg" {
+  name        = "example-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.example_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example-sg"
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.example_subnet.id
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
 
   tags = {
     Name = var.instance_name
   }
 }
+
+data "aws_availability_zones" "available" {}
