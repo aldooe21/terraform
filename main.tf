87cffa50ffc23a8644ac1e43a353d491e095e15b
Aldo <livio.gieraldo@izeno.com>
2025-05-05 13:57:44 +0700

    test3

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Assign existing VPC 
+# Gunakan VPC yang sudah ada
 data "aws_vpc" "existing" {
   filter {
     name   = "tag:Name"
@@ -10,11 +10,27 @@ data "aws_vpc" "existing" {
   }
 }
 
-# Assign existing security group 
+# Ambil AZ dari region saat ini
+data "aws_availability_zones" "available" {}
+
+# Buat subnet di VPC tersebut
+resource "aws_subnet" "example_subnet" {
+  vpc_id                  = data.aws_vpc.existing.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "example-subnet"
+  }
+}
+
+# Instance menggunakan subnet dari VPC custom, dan SG yang sudah ada
 resource "aws_instance" "example" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
+  subnet_id              = aws_subnet.example_subnet.id
   vpc_security_group_ids = ["sg-06d0fddd431f3e114"]
 
   tags = {
@@ -24,18 +40,13 @@ resource "aws_instance" "example" {
 
 # Output
 output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.example.public_ip
+  value = aws_instance.example.public_ip
 }
 
 output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.example.id
+  value = aws_instance.example.id
 }
 
 output "vpc_id" {
-  description = "ID of the VPC"
-  value       = data.aws_vpc.existing.id
+  value = data.aws_vpc.existing.id
 }
-
-data "aws_availability_zones" "available" {}
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
   value       = aws_instance.example.public_ip
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = data.aws_vpc.existing.id
 }
