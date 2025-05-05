3c6b7e1e93ca1bbfee6603f548befc768dd9fed9
Aldo <livio.gieraldo@izeno.com>
2025-05-05 14:01:47 +0700

    test5

--- main.tf
+++ main.tf
@@ -10,28 +10,20 @@ data "aws_vpc" "existing" {
   }
 }
 
-# Ambil AZ dari region saat ini
-data "aws_availability_zones" "available" {}
-
-# Buat subnet di VPC tersebut
-resource "aws_subnet" "example_subnet" {
-  vpc_id                  = data.aws_vpc.existing.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[0]
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "example-subnet"
-  }
+# Gunakan subnet yang sudah ada
+data "aws_subnet" "existing_subnet" {
+  id = "subnet-07a5355f2120f2ef5"  # subnet public yang ada
 }
 
-# Instance menggunakan subnet dari VPC custom, dan SG yang sudah ada
+# Instance menggunakan subnet dan security group yang sudah ada
 resource "aws_instance" "example" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
-  subnet_id              = aws_subnet.example_subnet.id
+  subnet_id              = data.aws_subnet.existing_subnet.id
   vpc_security_group_ids = ["sg-06d0fddd431f3e114"]
+
+  associate_public_ip_address = true  # jika subnet tidak map IP publik otomatis
 
   tags = {
     Name = var.instance_name
